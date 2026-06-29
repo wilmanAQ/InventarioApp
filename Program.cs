8b8b2e1d83ecf7a001a088bb830ae549da27d964
Wilman Quiñonez <waquinonezv@unipacifico.edu.co>
2026-06-28 19:14:14 -0500

    Menu de ayuda

--- Program.cs
+++ Program.cs
@@ -9,6 +9,28 @@ using System.Reflection;
 var assembly = Assembly.GetExecutingAssembly();
 var version = assembly.GetName().Version;
 
+if (args.Length > 0)
+{
+    switch (args[0])
+    {
+        case "--help":
+            MostrarAyuda();
+            Environment.Exit(0);
+            break;
+
+        case "--version":
+            Console.WriteLine($"Versión del ensamblado: {version}");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine($"Error: Comando desconocido: '{args[0]}'");
+            Console.WriteLine($"Use '--help' para ver la lista de comandos disponibles.");
+            MostrarAyuda();
+            Environment.Exit(1);
+             break;
+    
+    }      
+}
 
 Console.WriteLine("==========================================");
 Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
@@ -31,3 +53,16 @@ Console.WriteLine("    └── src/ ");
 Console.WriteLine("     └── Modulos/ (Proxima claese)");
 Console.WriteLine();
 Console.WriteLine("Proxiomo paso: Agregar Checkpoint");
+
+void MostrarAyuda()
+{
+    Console.WriteLine("USO: InventarioApp [comando] [opciones]");
+    Console.WriteLine();
+    Console.WriteLine("COMANDOS:");
+    Console.WriteLine("  --help, -h      Muestra esta ayuda");
+    Console.WriteLine("  --version, -v   Muestra la version del programa");
+    Console.WriteLine();
+    Console.WriteLine("EJEMPLOS:");
+    Console.WriteLine(" dotnet run -- --help");
+    Console.WriteLine(" dotnet run -- --version");
+}
